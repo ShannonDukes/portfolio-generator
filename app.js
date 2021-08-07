84b8751cfcdbd3b38a40b96d4006abcc742f33bf
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-08-07 14:11:39 -0700

    updates to all pages, correct function for html page creation

--- app.js
+++ app.js
@@ -21,9 +21,6 @@
 // console.log(profileDataArgs);
 // node app.js 'Shannon Dukes' 'Web Developer' into command line
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
 //const printProfileData = (profileDataArr) => {
 //    console.log(profileDataArr);
 //  };
@@ -37,22 +34,45 @@ console.log(profileDataArgs);
 //    }
 //  };
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+// This...
+//    for (let i = 0; i < profileDataArr.length; i += 1) {
+//      console.log(profileDataArr[i]);
+//    }
 
-    console.log('================');
+//    console.log('================');
 
-    // Is the same as this...
-    //    profileDataArr.forEach((profileItem) => {
-    //      console.log(profileItem)
-    //    });
-    //  };
+// Is the same as this...
+//    profileDataArr.forEach(profileItem => console.log(profileItem));
+//  };
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
+// Is the same as this...
+//    profileDataArr.forEach((profileItem) => {
+//      console.log(profileItem)
+//    });
+//  };
 
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 // node app.js 'Shannon Dukes' 'Web Developer' into command line
+
+//const generatePage = (userName, githubName) => {
+//    return `
+//      Name: ${userName}
+//      GitHub: ${githubName}
+//   `;
+//};
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
