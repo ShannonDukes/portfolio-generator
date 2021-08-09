dcb3e9573510bd315a0c2b71486116b301b44680
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-08-08 17:11:57 -0700

    updates to app js file and promptProject call

--- app.js
+++ app.js
@@ -63,16 +63,133 @@
 //   `;
 //};
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// console.log(profileDataArgs);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// console.log(inquirer)
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?'
+//     }
+//   ])
+
+// const promptUser = () => {
+//     return inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'name',
+//         message: 'What is your name?'
+//       },
+//       {
+//         type: 'input',
+//         name: 'github',
+//         message: 'Enter your GitHub Username'
+//       },
+//       {
+//         type: 'input',
+//         name: 'about',
+//         message: 'Provide some information about yourself:'
+//       }
+//     ]);
+//   };
+const inquirer = require('inquirer');
+//const promptProject = portfolioData.projects = [];
+// If there's no 'projects' array property, create one
+.then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+    }
+]);
+};
+// promptUser().then(answers => console.log(answers));
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
+
+//promptUser()
+    // .then(answers => console.log(answers))
+    // .then(promptProject)
+    // .then(projectAnswers => console.log(projectAnswers));
+
+  // node app.js 'Shannon Dukes' 'Web Developer' into command line
